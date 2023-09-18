39a9a919df5ae15c1a5bc0f7a5c1bcfd976f7caa
yixiangD <yxdeng05@gmail.com>
2023-09-18 15:53:14 -0400

    init kmeans

--- R/nhanes.R
+++ R/nhanes.R
@@ -1,27 +1,54 @@
 # load data
 library(dplyr)
-path <- "~/Downloads/Michail"
-df <- SASxport::read.xport(paste(path, "P_DEMO.XPT", sep = "/"))
+path <- "~/Downloads/michail2"
+df <- readxl::read_excel(paste(path, "full_data.xlsx", sep = "/"))
+# drop redundant columns
+data <- df[, !grepl("\\.", names(df))]
 
-files <-
-  c(
-    "P_BMX.XPT",
-    "P_LUX.XPT",
-    "P_BIOPRO.XPT",
-    "P_CBC.XPT",
-    "P_GLU.XPT",
-    "P_GHB.XPT",
-    "P_MCQ.XPT",
-    "P_BPQ.XPT",
-    "P_BPXO.XPT",
-    "P_TRIGLY.XPT",
-    "P_HDL.XPT",
-    "P_DIQ.XPT",
-    "P_INS.XPT",
-    "P_HEQ.XPT",
-    "P_DR1TOT.XPT"
-  )
-for (i in files) {
-  temp <- SASxport::read.xport(paste(path, i, sep = "/"))
-  df <- merge(df, temp, by = intersect(names(temp), names(df)))
+# build unsupervised learning models
+col.id <- c("SEQN")
+col.lab <- c("MASLD", "Cryptogenic", "MetALD", "FIB4", "APRI", "HOMAIR", "ION", "fibrosis")
+col.bin.lab <- c("MASLD", "MetALD")
+# TODO:HERE may be feature engineering to get a subset
+col.feat <- colnames(data)[!colnames(data) %in% c(col.id, col.lab)]
+
+# Load Required Libraries
+library(dplyr) # For data manipulation
+library(ggplot2) # For data visualization
+library(FactoMineR) # For PCA
+library(cluster) # For K-means clustering
+
+# Load and Preprocess Data
+numeric_data <- data[, col.feat]
+
+# Perform PCA
+pca_result <- FactoMineR::PCA(numeric_data, scale.unit = TRUE)
+# Select the Number of Components
+fig.pca <- plot(pca_result, choix = "var")
+ggsave(paste(path, "pca.pdf", sep = "/"), fig.pca)
+# Decide the number of components to keep (e.g., K)
+K <- 2
+# Extract PCA Components
+selected_pca <- pca_result$ind$coord[, 1:K]
+
+# Perform K-means Clustering
+wss <- numeric(10)
+
+for (i in 1:10) {
+  kmeans_model <- kmeans(selected_pca, centers = i)
+  wss[i] <- kmeans_model$tot.withinss
+}
+
+# Plot the Elbow curve to choose an appropriate K
+
+# Choose K based on the plot
+final_kmeans_model <- kmeans(selected_pca, centers = K)
+data$cluster <- final_kmeans_model$cluster
+data <- cbind(data, selected_pca)
+# Visualize the Results
+for (lab in col.bin.lab) {
+  fig <- ggplot(data, aes(Dim.1, Dim.2, color = factor(!!sym(lab)))) +
+    geom_point() +
+    labs(title = "PCA + K-means Clustering")
+  ggsave(paste(path, paste0(lab, "_pca+kmeans.pdf"), sep = "/"), fig)
 }
